525c254f5a0c973994cf80bf829e04bfe805b082
erdem koyuncu <erdemfb1907@outlook.com>
2025-08-27 20:04:05 +0300

    Moved messages folder to correct place, started building home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -30,7 +25,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased min-h-[100dvh] leading-relaxed`}
       >
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider
