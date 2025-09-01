6c318067df7da35de9c7cf2398452ea70064dd92
erdem koyuncu <erdemfb1907@outlook.com>
2025-09-01 09:24:15 +0300

    Added color, add clamp to everything

--- src/app/components/(fullscreen)/neue-haas-design/page.tsx
+++ src/app/components/(fullscreen)/neue-haas-design/page.tsx
@@ -7,27 +7,47 @@ import AnimatedLetters from "@/components/custom/animated-letters";
 export default function Page() {
   return (
     <ComponentWrapper>
-      <div className="h-[100dvh] sm:h-auto sm:min-h-[100dvh] transition-all duration-400 ease-out max-w-[1400px] mx-auto p-6 grid grid-cols-12 grid-rows-12 overflow-hidden">
-        <div className="col-span-12 row-start-1 row-span-9 flex flex-col items-center justify-center">
-          <AnimatedLetters text="aaaaaaaa" delay={0} />
-          <AnimatedLetters text="aaaaaaaa" delay={0.143} />
-          <AnimatedLetters text="aaaaaaaa" delay={0.285} />
-          <AnimatedLetters text="aaaaaaaa" delay={0.428} />
-          <AnimatedLetters text="aaaaaaaa" delay={0.571} />
-          <AnimatedLetters text="aaaaaaaa" delay={0.714} />
-          <AnimatedLetters text="aaaaaaaa" delay={0.857} />
-          <AnimatedLetters text="aaaaaaaa" delay={1} />
-        </div>
-        <div className="col-span-12 row-start-10 row-span-3 flex flex-col h-full justify-center font-neue-haas-display">
-          <div className="max-w-[33vw] py-4 w-full mx-auto flex">
-            <div className="leading-4.5 flex flex-col gap-[1.25vh]">
-              <p className="font-bold">
-                Neue Haas Grotesk
-                <br /> Display
-              </p>
-              <p>Symbol No. 001</p>
+      <div className="bg-special-orange text-neutral-100">
+        <div className="h-[100dvh] sm:h-auto sm:min-h-[100dvh] transition-all duration-400 ease-out max-w-[1400px] mx-auto p-6 md:pt-[clamp(0.1rem,3vw,3rem)] grid grid-cols-12 grid-rows-12 overflow-hidden">
+          <div className="col-span-12 row-start-1 row-span-9 flex flex-col items-center justify-center">
+            <AnimatedLetters text="aaaaaaaa" delay={0} />
+            <AnimatedLetters text="aaaaaaaa" delay={0.143} />
+            <AnimatedLetters text="aaaaaaaa" delay={0.285} />
+            <AnimatedLetters text="aaaaaaaa" delay={0.428} />
+            <AnimatedLetters text="aaaaaaaa" delay={0.571} />
+            <AnimatedLetters text="aaaaaaaa" delay={0.714} />
+            <AnimatedLetters text="aaaaaaaa" delay={0.857} />
+            <AnimatedLetters text="aaaaaaaa" delay={1} />
+          </div>
+          <div className="col-span-12 row-start-10 row-span-3 flex flex-col h-full lg:justify-center font-neue-haas-display">
+            <div className="max-w-[82vw] sm:max-w-[62vw] md:max-w-[53vw] lg:max-w-[clamp(1rem,33vw,41rem)] py-4 w-full mx-auto flex justify-between text-[2.7vw] leading-[2.7vw] sm:text-[1.7vw] sm:leading-[1.7vw] md:text-[1.6vw] md:leading-[1.6vw] lg:text-[clamp(0.1rem,0.9vw,1.2rem)] lg:leading-[clamp(0.1rem,0.9vw,1.2rem)] ">
+              <div className="flex flex-col justify-between">
+                <p className="font-bold">
+                  Neue Haas Grotesk
+                  <br /> Display
+                </p>
+                <p>Symbol No. 001</p>
+              </div>
+              <div className="pr-[clamp(0.1rem,4vw,5rem)] ">
+                <span className="font-[150] ">Ultra Thin 15 </span>
+                <span className="font-[250] ">Thin 25</span>
+                <br />
+                <span className="font-[350] ">Extra Light 35 </span>
+                <span className="font-[450] ">Light45</span>
+                <br />
+                <span className="font-[550] ">Roman 55 </span>
+                <span className="font-[650] ">Medium 65</span>
+                <br />
+                <span className="font-[750] ">Bold 75 </span>
+                <span className="font-[950] ">Black 95</span>
+              </div>
+              <div>
+                <div className="font-[450] ">Typeface by</div>
+                <div className="font-[750]">Max A. Miedinger</div>
+                <div className="font-[450]">Revived by</div>
+                <div className="font-[750]">Christian Schwartz</div>
+              </div>
             </div>
-            {/* <div className=""></div> */}
           </div>
         </div>
       </div>
