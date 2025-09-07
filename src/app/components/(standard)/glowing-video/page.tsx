42c5b9d0c7614bcee934b5bafd7c5f1c61cb897d
erdem koyuncu <erdemfb1907@outlook.com>
2025-09-07 17:38:14 +0300

    Added glowing video component

---
+++ src/app/components/(standard)/glowing-video/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+
+export default function Page() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+    if (!video || !canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    let animationFrame: number;
+
+    const render = () => {
+      if (video.paused || video.ended) return;
+      // draw current video frame to canvas
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      animationFrame = requestAnimationFrame(render);
+    };
+
+    const handlePlay = () => {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      render();
+    };
+
+    video.addEventListener("play", handlePlay);
+
+    return () => {
+      video.removeEventListener("play", handlePlay);
+      cancelAnimationFrame(animationFrame);
+    };
+  }, []);
+
+  return (
+    <div className="h-[100dvh] sm:h-auto sm:min-h-[100dvh] p-6 mx-auto overflow-hidden flex items-center justify-center relative">
+      {/* glowing canvas */}
+      <canvas
+        ref={canvasRef}
+        className="absolute mx-auto max-w-full w-full inset-0 h-full scale-125 blur-[300px] -z-10"
+      />
+      {/* video */}
+      <video
+        ref={videoRef}
+        src="/videos/example2.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="rounded-xl shadow-lg relative z-10 max-w-full w-[700px]"
+      />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,12 @@ export default function Home() {
         <div className="mt-26">
           <p className="font-medium tracking-tight">Projects</p>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 mt-5">
+            <Link href="/components/glowing-video">
+              <HomeCard className="flex items-center justify-center font-medium">
+                Glowing Video
+              </HomeCard>{" "}
+            </Link>
+
             <Link href="/components/squircles">
               <HomeCard className="flex items-center gap-3 justify-center font-medium">
                 Squircle UI Elements
